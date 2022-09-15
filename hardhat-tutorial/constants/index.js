3ddfaae8352cdf035c35e6fda8cb871f06f4085b
Nikhil <longestwavelength@protonmail.com>
2022-09-15 15:46:35 +0530

    "Finished this project"

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x753F3A6264C75D2763e6fa8E1AfbAa545e7BA469";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
